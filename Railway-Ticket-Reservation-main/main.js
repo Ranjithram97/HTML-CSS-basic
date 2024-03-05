8c03f85654fc291a8263502f7d311118c6bd1a89
Sriram Babu T <127068774+Ranjithram97@users.noreply.github.com>
2024-03-05 12:17:47 +0530

    Add files via upload

---
+++ Railway-Ticket-Reservation-main/main.js
@@ -0,0 +1,14 @@
+/* These Functions are for showing popup Windows */
+function trainB(){
+  document.getElementById("bookTrain").style.display = "flex";
+}
+function trainC(){
+  document.getElementById("bookTrain").style.display = "none";
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "flex";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
